1286b4ea4d4cfd03172d0be5e6af1e76d18117ca
Daria <lo.pennequin@gmail.com>
2018-11-05 02:29:51 +0100

    clean up after noob merge

--- src/server/middlewares/logRequest.js
+++
@@ -1,7 +0,0 @@
-const logger = require('./../logger/logger.js')(module);
-
-module.exports = (req, res, next) => {
-    logger.info(`REST API request : ${req.method} | ${req.url}`);
-    logger.info('===================================');
-    next();
-};
